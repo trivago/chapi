e22a2b57b9cb923c09d073dc277b46e92f2838e4
msiebeneicher <marc.siebeneicher@trivago.com>
2015-08-26 22:49:35 +0200

    added simple JobRepositoryFileSystemTest

--- test/unit/Service/JobRepository/JobRepositoryFileSystemTest.php
+++ test/unit/Service/JobRepository/JobRepositoryFileSystemTest.php
@@ -1,18 +1,43 @@
 <?php
 /**
- * @package: orchestra-
+ * @package: chapi
  *
  * @author:  msiebeneicher
  * @since:   2015-08-21
- *
- * @link:    http://
  */
 
 
 namespace unit\Service\JobRepository;
 
 
-class JobRepositoryFileSystemTest
-{
+use Chapi\Service\JobRepository\JobRepositoryFileSystem;
 
+class JobRepositoryFileSystemTest extends \PHPUnit_Framework_TestCase
+{
+    /** @var \Prophecy\Prophecy\ObjectProphecy */
+    private $oFileSystemService;
+
+    /** @var \Prophecy\Prophecy\ObjectProphecy */
+    private $oCache;
+
+    /** @var string */
+    private $sRepositoryDir = '/var/test/';
+
+    public function setUp()
+    {
+        $this->oFileSystemService = $this->prophesize('Symfony\Component\Filesystem\Filesystem');
+
+        $this->oCache = $this->prophesize('Chapi\Component\Cache\CacheInterface');
+    }
+
+    public function testCreateInstance()
+    {
+        $_oFileSystemRepository = new JobRepositoryFileSystem(
+            $this->oFileSystemService->reveal(),
+            $this->oCache->reveal(),
+            $this->sRepositoryDir
+        );
+
+        $this->assertInstanceOf('Chapi\Service\JobRepository\JobRepositoryServiceInterface', $_oFileSystemRepository);
+    }
 }
