951f8035ed32f907769e09b86a07260aa740f06d
msiebeneicher <marc.siebeneicher@trivago.com>
2017-03-22 17:38:27 +0100

    Switched to new chap config class to get ignore settings

--- test/unit/Service/JobRepository/Filter/FilterIgnoreSettingsTest.php
+++ test/unit/Service/JobRepository/Filter/FilterIgnoreSettingsTest.php
@@ -13,8 +13,6 @@ namespace unit\Service\JobRepository\Filter;
 use Chapi\Service\JobRepository\Filter\FilterIgnoreSettings;
 use ChapiTest\src\TestTraits\AppEntityTrait;
 use ChapiTest\src\TestTraits\JobEntityTrait;
-use org\bovigo\vfs\vfsStream;
-use org\bovigo\vfs\vfsStreamDirectory;
 
 class FilterIgnoreSettingsTest extends \PHPUnit_Framework_TestCase
 {
@@ -24,39 +22,28 @@ class FilterIgnoreSettingsTest extends \PHPUnit_Framework_TestCase
     /** @var \Prophecy\Prophecy\ObjectProphecy */
     private $oLogger;
 
-    /** @var string */
-    private $sRepositoryDir = 'FilterIgnoreSettingsTestDir';
-
-    /** @var string  */
-    private $sTempTestDir = '';
-
-    /** @var  vfsStreamDirectory */
-    private $oVfsRoot;
+    /** @var \Prophecy\Prophecy\ObjectProphecy */
+    private $oChapiConfig;
 
     public function setUp()
     {
         $this->oLogger = $this->prophesize('Psr\Log\LoggerInterface');
 
-        $_aStructure = array(
-            '.chapiignore' => "^test-123.*\n/test/123/.*"
-        );
-
-        $this->oVfsRoot = vfsStream::setup($this->sRepositoryDir, null, $_aStructure);
-
-        // init and set up temp directory
-        $_sTempTestDir = sys_get_temp_dir();
-        $this->sTempTestDir = $_sTempTestDir . DIRECTORY_SEPARATOR . 'ChapiUnitTest';
-        if (!is_dir($this->sTempTestDir))
-        {
-            mkdir($this->sTempTestDir, 0755);
-        }
+        $this->oChapiConfig = $this->prophesize('Chapi\Component\Config\ChapiConfigInterface');
+        $this->oChapiConfig->getProfileConfig()->willReturn([
+            'ignore' => [
+                'test-123*',
+                '/test/123/*',
+                '!test-123-ignore-not'
+            ]
+        ]);
     }
 
     public function testIgnoreRules()
     {
         $_oFilter = new FilterIgnoreSettings(
-            [vfsStream::url($this->sRepositoryDir)],
-            $this->oLogger->reveal()
+            $this->oLogger->reveal(),
+            $this->oChapiConfig->reveal()
         );
 
 
